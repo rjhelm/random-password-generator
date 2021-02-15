22091754cacbf1818d5e68862cf9a783470936f8
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-14 18:38:49 -0700

    fixed small errors/notes for clarity

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,10 @@ let validSpecialCharacters = ['#','$','%','&','(',')','*','+',',','-','.','/',':
 // Allow generate button to function and display the generated password
 let generateBtn = document.querySelector("#generate");
 
+// Generate button now starts on click and displays password
 generateBtn.addEventListener("click", function () {
-  ps = generatePassword();
-  document.getElementById("password").placeholder = ps;
+  userPassword = generatePassword();
+  document.getElementById("password").placeholder = userPassword;
 });
 
 // Add prompts for the user to select length and characters
@@ -23,15 +24,15 @@ function getCharacterOptions() {
     return null;
   }
   
-  // Prompt for lowercase
+  // Ask user what characters they would like to use in password
   let useLowerCaseCharacters = confirm("Would you like your password to include lowercase letters?");
-  // Prompt for uppercase
-  let useUpperCaseCharacters = confirm("Would you like your password to include uppercase letters?");
-  // Prompt for numbers
-  let useNumbers = confirm("Would you like your password to include numbers?");
-  // Prompt for special characters
-  let useSpecialCharacters = confirm("Would you like your password to include special characters?");
   
+  let useUpperCaseCharacters = confirm("Would you like your password to include uppercase letters?");
+  
+  let useNumbers = confirm("Would you like your password to include numbers?");
+  
+  let useSpecialCharacters = confirm("Would you like your password to include special characters?");
+    // Alerts the user that they must select at least 1 and restarts the prompts
     if (!useLowerCaseCharacters && !useUpperCaseCharacters && !useNumbers && !useSpecialCharacters) {
       alert("Your must select at least 1 of the previous options. Please try again.");
       return null;
@@ -47,37 +48,44 @@ function getCharacterOptions() {
 
 }
 
-// Generates the password based on the user's choices
+// Function to generate password based on users answers 
 function generatePassword () {
 
+  // Define the above info to know what characters we are randomizing and the length
   let characterOptions = getCharacterOptions();
  
   if (!characterOptions) return;
 
+  // Use the answer to prompt and generate password with those values
   let validPasswordCharacters = [];
 
+  // Lowercase Characters
   if (characterOptions.useLowerCaseCharacters) {
     validPasswordCharacters = validPasswordCharacters.concat(validLowerCaseCharacters)
   }
     
+  // Uppercase Characters
   if (characterOptions.useUpperCaseCharacters) {
     validPasswordCharacters = validPasswordCharacters.concat(validUpperCaseCharacters)
   }
 
+  // Numbers
   if (characterOptions.useNumbers) {
     validPasswordCharacters = validPasswordCharacters.concat(validNumbers)
   }
 
-  if (characterOptions.useLowerCaseCharacters) {
+  // Special Characters
+  if (characterOptions.useSpecialCharacters) {
     validPasswordCharacters = validPasswordCharacters.concat(validSpecialCharacters)
   }
 
   let generatedPassword = [];
-
+  // For loop to randomize based on the user's characters
   for (let i = 0; i < characterOptions.passwordLength; i++) {
     let randomGenerate = Math.floor(
       Math.random() * Math.floor(validPasswordCharacters.length)
     );
+    
     generatedPassword.push(validPasswordCharacters[randomGenerate]);
   }
 
@@ -86,8 +94,8 @@ function generatePassword () {
   return userPassword;
 }
 
-function UserInput(ps) {
-  document.getElementById("password").textContent = ps;
+function UserInput(userPassword) {
+  document.getElementById("password").textContent = userPassword;
 }    
   
 
